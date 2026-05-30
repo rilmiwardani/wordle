cf5a2b813440b6e573b03cc65cb96dd8761931fc
Seka Ludianto <sekaludianto@gmail.com>
2026-05-30 20:32:51 +0700

    Fix Error tt-target-idc is required by placing it in root options as ttTargetIdc

--- server.js
+++ server.js
@@ -256,9 +256,7 @@ async function connectToTikTok(uniqueId, sessionId = null) {
       enableExtendedGiftInfo: true,
       enableWebsocketUpgrade: true,
       requestPollingIntervalMs: 2000,
-      clientParams: {
-        "tt-target-idc": "alisg",
-      },
+      ttTargetIdc: "alisg",
       requestHeaders: {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
